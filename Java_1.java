b43eec02b0c7b86d05af1ea0ab76d382fdc790cb
Josseluu <jslslpz@myuax.com>
2024-02-08 10:39:00 +0100

    -

---
+++ Java_1.java
@@ -0,0 +1,22 @@
+public class Ejercicio_1 {
+}
+public static void main (String[] args) {
+    int vi = 0;
+    double vd = 0;
+    char vc = 'a';
+    String vcad = "Hello";
+    boolean vb = true;
+    System.out.println("vi = " + vi);
+    System.out.println("vd = " + vd);
+    System.out.println("vc = " + vc);
+    System.out.println("vcad = " + vcad);
+    System.out.println("vb = " + vb);
+
+    vi = 2 + 4 * 4 / 5;
+    System.out.println("vi = " + vi);
+    vi = (int) (2.5 + 4.8);
+    System.out.println("vi = " + vi);
+    vd = 2 + 4 * 4 / 5;
+    System.out.println("vd = " + vd);
+}
+
